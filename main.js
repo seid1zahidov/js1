b4227c623bdb76fff50431058d1abeb887f2b4a6
seid1zahidov <seid.zahidov15@gmail.com>
2025-05-05 21:49:21 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// let count:number = 0
+// while( count < 5){
+//     count = count +1
+//     console.log(count)
+// }
+// let reqem1: number = 10
+// let reqem2: number = 20
+// let count = reqem1+reqem2
+// console.log(count)
+// let reqem1: number = 10
+// let reqem2: number = 20
+// let count = reqem1*reqem2
+// console.log(count)
+// let daxilEdilenEded: number 
+var count = Number(prompt());
+alert(count);
+console.log(count);
---
+++ main.ts
@@ -0,0 +1,34 @@
+// let count:number = 0
+
+// while( count < 5){
+//     count = count +1
+//     console.log(count)
+// }
+
+// let reqem1: number = 10
+// let reqem2: number = 20
+
+// let count = reqem1+reqem2
+// console.log(count)
+
+// let reqem1: number = 10
+// let reqem2: number = 20
+
+// let count = reqem1*reqem2
+// console.log(count)
+
+// let daxilEdilenEded: number 
+
+// let count = Number(prompt())
+
+// alert(count)
+// console.log(count)
+
+
+
+
+
+
+
+
+
